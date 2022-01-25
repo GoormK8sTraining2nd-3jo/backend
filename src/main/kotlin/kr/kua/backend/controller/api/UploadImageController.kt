9773050d5df1019008e59db21ec64acfe216e4e9
HoYoung <kua@kakao.com>
2022-01-25 17:47:49 +0900

    Update ImageUpload Controller

--- src/main/kotlin/kr/kua/backend/controller/api/UploadImageController.kt
+++ src/main/kotlin/kr/kua/backend/controller/api/UploadImageController.kt
@@ -1,6 +1,9 @@
 package kr.kua.backend.controller.api
 
+import kr.kua.backend.model.response.BaseResult
+import kr.kua.backend.service.response.ResponseService
 import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/api/image/"])
 class UploadImageController {
 
-    @PostMapping("/upload")
-    fun ImageUpload(@RequestBody image: String): {
+    @Autowired
+    lateinit var responseService: ResponseService
 
+    @PostMapping("/upload")
+    fun ImageUpload(@RequestBody image: String): BaseResult {
+
+        return responseService.successResult
     }
 }
 // 웅상 20일에
